a92c78511e8b8f57688c7072c6cfbe467c1e344b
Moharram Moharrami <moharram.m98@gmail.com>
2022-01-03 14:35:53 +0330

    put app.js and chat feed together

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
 const App = () => {
@@ -9,6 +11,7 @@ const App = () => {
             projectID = "7195d1f3-c36a-47f5-b433-ed062af3e731"
             userName = "mafan"
             userSecret = "123456"
+            renderChatFeed = {(chatAppProps) => <ChatFeed {... chatAppProps} />}
         />
     );
 }
