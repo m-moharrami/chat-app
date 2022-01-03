358bc7830a393e14059e8a25eaf2a3b815e773db
Moharram Moharrami <moharram.m98@gmail.com>
2022-01-03 17:00:46 +0330

    handle others messages including text and images

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,28 @@
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+
     return (
-        <div>
-            TheirMessage
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div 
+                    className="message-avatar"
+                    style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+                />
+            )}
+            {message?.attachments?.length > 0
+                ? (
+                    <img 
+                        src={message.attachments[0].file}
+                        alt="message-attachment"
+                        className="message-image"
+                        style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                    />
+                ) : (
+                    <div className="messages" style={{float: 'left', backgroundColor: '#CABCDC' }}>
+                        {message.text}
+                    </div>
+                )
+            }
         </div>
     );
 }
