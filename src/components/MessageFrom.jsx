0be9a659aa720b0b784248e2b419939588897727
Moharram Moharrami <moharram.m98@gmail.com>
2022-01-22 12:46:12 +0330

    add ability to send pictures

--- src/components/MessageFrom.jsx
+++ src/components/MessageFrom.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
 const MessageForm = (props) => {
     const [value, setValue] = useState('');
@@ -20,6 +21,10 @@ const MessageForm = (props) => {
 
         isTyping(props, chatId);
     }
+
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, { files: event.target.files, text: '' });
+    }
     
     return (
         <form className="message-form" onSubmit={handleSubmit}>
@@ -30,6 +35,21 @@ const MessageForm = (props) => {
                 onChange={handleChange}
                 onSubmit={handleSubmit}
             />
+            <label htmlFor="upload-button">
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon" />
+                </span>
+            </label>
+            <input 
+                type="file"
+                multiple={false}
+                id="upload-button"
+                style={{ display: 'none' }}
+                onChange={handleUpload}
+            />
+            <button type="submit" className="send-button">
+                <SendOutlined className="send-icon" />
+            </button>
         </form>
     );
 }
